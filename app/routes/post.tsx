bacd97e788b4d71a48e25ea07308862a3d3010cb
Amy Y <ctrlalternativedelete@gmail.com>
2026-03-20 01:57:05 +1100

    fix: tags on posts

--- app/routes/post.tsx
+++ app/routes/post.tsx
@@ -4,10 +4,11 @@ import oembedTransformer from '@remark-embedder/transformer-oembed';
 import { useMdxComponent, useMdxAttributes } from 'react-router-mdx/client'
 import type { Route } from "./+types/post";
 import { videoExtensions } from "~/lib/consts";
+import { Badge } from "~/components/ui/badge";
 
 function MdxImg(props: React.ComponentProps<"img">) {
     const extension = (props.src ?? "").split(".", 2).at(1)?.toLowerCase();
-    
+
     if (extension != null && videoExtensions.includes(extension)) {
         return (
             <video
@@ -39,13 +40,18 @@ export async function loader({ request }: Route.LoaderArgs) {
 }
 
 export default function Route() {
-    const Component = useMdxComponent({ img: MdxImg })
+    const Component = useMdxComponent({ img: MdxImg });
     const attributes = useMdxAttributes()
 
     return <section className='flex items-center justify-center w-full p-3'>
-        <div className='prose max-w-7xl'>
-            <h1><mark className='bg-primary text-primary-foreground'>{attributes.title}</mark></h1>
-            <Component />
+        <div className="max-w-7xl space-y-3">
+            <h1 className="text-3xl"><mark className='bg-primary text-primary-foreground'>{attributes.title}</mark></h1>
+            <div className="flex gap-1 overflow-hidden w-full">
+                {(attributes.tags as unknown as string[] ?? []).map((e) => <Badge>{e}</Badge>)}
+            </div>
+            <div className='prose max-w-7xl'>
+                <Component />
+            </div>
         </div>
     </section>;
 
