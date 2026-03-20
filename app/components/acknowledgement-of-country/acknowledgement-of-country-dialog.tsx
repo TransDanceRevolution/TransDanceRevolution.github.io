3fa1463d909b2d7d93cdb524b03319d3728882b5
Amy Y <ctrlalternativedelete@gmail.com>
2026-03-21 01:00:07 +1100

    fix: disabled close button

--- app/components/acknowledgement-of-country/acknowledgement-of-country-dialog.tsx
+++ app/components/acknowledgement-of-country/acknowledgement-of-country-dialog.tsx
@@ -4,8 +4,8 @@ import AcknowledgementOfCountryContent from "./acknowledgement-of-country-conten
 import type { DialogRoot } from "@base-ui/react";
 
 export default function AcknowledgementOfCountryDialog(props: Omit<DialogRoot.Props, "children">) {
-    return <Dialog disablePointerDismissal={true} {...props}>
-        <DialogContent className="sm:max-w-md">
+    return <Dialog disablePointerDismissal={true} modal={true} {...props}>
+        <DialogContent showCloseButton={false} className="sm:max-w-md">
             <DialogHeader>
                 <DialogTitle>Acknowledgment of Country</DialogTitle>
                 <DialogDescription className={"prose pt-3"}>
