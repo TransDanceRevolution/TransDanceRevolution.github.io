95292fdd15838c60314af6d3249bc18cac138cc8
Amy Y <ctrlalternativedelete@gmail.com>
2026-04-08 00:15:29 +1000

    fix: client lifetime for tina

--- app/lib/hooks.ts
+++ app/lib/hooks.ts
@@ -2,6 +2,13 @@ import React from "react";
 import { ExperimentalGetTinaClient } from "~/../tina/__generated__/types";
 
 export function useAdminTinaClient() {
-    const [client] = React.useState(() => ExperimentalGetTinaClient());
+    const [client, setClient] = React.useState<ReturnType<typeof ExperimentalGetTinaClient> | null>(null);
+    React.useEffect(() => {
+        try {
+            setClient(ExperimentalGetTinaClient());
+        } catch (error) {
+            console.log("Tina client was not initialized: ", error);
+        }
+    }, []);
     return client;
 }
