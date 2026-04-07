b7cfa25a747790f36d43ab7495b4048266c1c1d2
Amy Y <ctrlalternativedelete@gmail.com>
2026-04-08 01:24:00 +1000

    fix: post errors

--- app/routes/post.tsx
+++ app/routes/post.tsx
@@ -1,6 +1,6 @@
 import { useTina } from "tinacms/dist/react"
 import type { Route } from "./+types/post"
-import { useLoaderData } from "react-router"
+import { isRouteErrorResponse, useLoaderData } from "react-router"
 import PostSection from "~/components/post/post-section"
 
 export async function loader({ params }: Route.LoaderArgs) {
@@ -10,7 +10,7 @@ export async function loader({ params }: Route.LoaderArgs) {
 }
 
 export const meta: Route.MetaFunction = ({ loaderData }) => {
-  const title = `${loaderData.data.post.title} - Trans Dance Revolution Blog`
+  const title = `${loaderData?.data.post.title ?? "Unknown"} - Trans Dance Revolution Blog`
   return [
     { title },
     {
@@ -19,6 +19,33 @@ export const meta: Route.MetaFunction = ({ loaderData }) => {
         "Trans Dance Revolution Blog, from Naarm, Gadigal, to the world.",
     },
   ]
+}
+
+export function ErrorBoundary({
+  error,
+}: Route.ErrorBoundaryProps) {
+  console.log("post error");
+  if (isRouteErrorResponse(error)) {
+    return (
+      <>
+        <h1>
+          {error.status} {error.statusText}
+        </h1>
+        <p>{error.data}</p>
+      </>
+    );
+  } else if (error instanceof Error) {
+    return (
+      <div>
+        <h1>Error</h1>
+        <p>{error.message}</p>
+        <p>The stack trace is:</p>
+        <pre>{error.stack}</pre>
+      </div>
+    );
+  } else {
+    return <h1>Unknown Error</h1>;
+  }
 }
 
 export default function Route() {
