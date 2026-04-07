5c506c4d2ba0dc0277df20f4800041e4e5389eaa
Amy Y <ctrlalternativedelete@gmail.com>
2026-04-07 23:42:35 +1000

    fix: 404

--- app/routes/404.tsx
+++ app/routes/404.tsx
@@ -1,13 +1,6 @@
-import { useLocation } from "react-router"
-
+// placeholder route to make gh pages happy
 export default function FourOhFour() {
-    const { pathname } = useLocation();
-    console.log(pathname);
     return (
-        <div className="max-w-7xl mx-auto p-3">
-            <h2>
-                404! Not Found!
-            </h2>
-        </div>
+        <></>
     )
 }
