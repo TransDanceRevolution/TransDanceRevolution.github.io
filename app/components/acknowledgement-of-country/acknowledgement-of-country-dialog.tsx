b375c1ad0b34d1c9373950f3a55a8f510e76f1b5
amy <ctrlalternativedelete@gmail.com>
2026-06-10 16:40:15 +1000

    feat: countdown for acknowledgment of country

--- app/components/acknowledgement-of-country/acknowledgement-of-country-dialog.tsx
+++ app/components/acknowledgement-of-country/acknowledgement-of-country-dialog.tsx
@@ -1,5 +1,5 @@
 import { Button } from "~/components/ui/button"
-import { Dialog as BaseDialog } from "@base-ui/react";
+import { Dialog as BaseDialog } from "@base-ui/react"
 import {
   Dialog,
   DialogClose,
@@ -12,15 +12,52 @@ import {
 } from "~/components/ui/dialog"
 import AcknowledgementOfCountryContent from "./acknowledgement-of-country-content.mdx"
 import type { DialogRoot } from "@base-ui/react"
-import React from "react";
+import React from "react"
 
 export default function AcknowledgementOfCountryDialog(
   props: Omit<DialogRoot.Props, "children">
 ) {
-  const dialogHandle = React.useMemo(() => BaseDialog.createHandle() ?? props.handle, [props.handle]);
-  const isDialogOpen = dialogHandle.store.useState("open");
+  const dialogHandle = React.useMemo(
+    () => BaseDialog.createHandle() ?? props.handle,
+    [props.handle]
+  )
+  const isDialogOpen = dialogHandle.store.useState("open")
+  const timerRef = React.useRef<number>(undefined)
+  const [remainingSecs, setRemainingSecs] = React.useState(0)
+  const clearTimerRefCb = React.useCallback(() => {
+    if (timerRef == null) {
+      return
+    }
+    window.clearTimeout(timerRef.current)
+    timerRef.current = undefined
+  }, [timerRef])
+  React.useEffect(() => {
+    if (isDialogOpen) {
+      let remainingSecs_ = 3
+      const timeoutCb = () => {
+        if (timerRef.current != null) {
+          remainingSecs_--
+        }
+        setRemainingSecs(remainingSecs_)
+        if (remainingSecs_ <= 0) {
+          timerRef.current = undefined
+          return
+        }
+        timerRef.current = window.setTimeout(timeoutCb, 1000)
+      }
+      timeoutCb()
+    } else {
+      clearTimerRefCb()
+    }
+    return clearTimerRefCb
+  }, [isDialogOpen])
   return (
-    <Dialog handle={dialogHandle} disablePointerDismissal={true} modal={true} {...props}>
+    <Dialog
+      handle={dialogHandle}
+      disablePointerDismissal={true}
+      modal={true}
+      {...props}
+    >
       <DialogContent showCloseButton={false} className="sm:max-w-md">
         <DialogHeader>
           <DialogTitle>Acknowledgment of Country</DialogTitle>
@@ -29,7 +66,16 @@ export default function AcknowledgementOfCountryDialog(
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <DialogClose render={<Button>Understood</Button>} />
+          <DialogClose
+            disabled={remainingSecs != 0}
+            render={
+              <Button>
+                {["Understood", remainingSecs != 0 ? `(${remainingSecs})` : []]
+                  .flatMap((e) => e)
+                  .join(" ")}
+              </Button>
+            }
+          />
         </DialogFooter>
       </DialogContent>
     </Dialog>
